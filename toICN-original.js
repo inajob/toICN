4504828aa02e6d2cbd6edc11a7f43735f9350544
inajob <ina_job@hotmail.com>
2021-09-02 20:06:21 +0900

    show unsupported modifier, support 7sus6, dim7, m7-5, add9

--- toICN-original.js
+++ toICN-original.js
@@ -10,12 +10,14 @@ Array.prototype.slice.bind(document.getElementsByClassName("chord"))().concat(Ar
     let noMap = {"C":1, "D":2, "E":3, "F":4, "G":5, "A":6, "B":7};
     let no = noMap[base[0]];
     let isQAvailable = false;
+    let unSupported = false;
     let isSharp = false;
     if("C,Dm,Em,F,G,Am,Bm".split(",").includes(base)){};
     if("Cm,D,E,Fm,Gm,A,B".split(",").includes(base)){swapped = true;};
     if(sharp == "#"){isSharp = true;};
-    if("dim,7,9,M7,sus4,aug".split(",").includes(q)){isQAvailable = true;};
-    s = no + (isSharp?"[#]":"") + (swapped?"~":"") + (isQAvailable?("["+q+"]"):"");
+    q = q.replace("7sus4","sus4").replace("dim7","dim").replace("7-5","m7-5");
+    if("dim,7,add9,M7,sus4,aug,m7-5".split(",").includes(q)){isQAvailable = true;}else{if(q.length>0){unSupported = true;}};
+    s = no + (isSharp?"[#]":"") + (swapped?"~":"") + (isQAvailable?("["+q+"]"):"" + (unSupported?"[!!"+q+"!!]":""));
     e.firstChild.nodeValue = s;
   };
 })
