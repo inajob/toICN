7d0dcf852358f15ab92ec70fb8f7b2e2bfb3dd4f
nonoyu <tu@ts-uc.org>
2021-09-03 07:33:08 +0900

    Editied pattern match (flat etc...)

--- toICN-original.js
+++ toICN-original.js
@@ -1,10 +1,10 @@
 Array.prototype.slice.bind(document.getElementsByClassName("chord"))().concat(Array.prototype.slice.bind(document.getElementsByTagName("rt"))()).forEach((e) => {
   let raw = ""+ e.firstChild.nodeValue;
-  let m = raw.match(/^([A-G](#{0,1})(m{0,1}))([^/]*)/);
+  let m = raw.match(/^([A-G])(#|b{0,1})(m{0,1})([^/]*)/);
   let s = "-";
   if(m){
-    let base = m[1];
-    let sharp = m[2];
+    let base = m[1]+m[3];
+    let keySignature = m[2];
     let q = m[4];
     let swapped = false;
     let noMap = {"C":1, "D":2, "E":3, "F":4, "G":5, "A":6, "B":7};
@@ -14,7 +14,7 @@ Array.prototype.slice.bind(document.getElementsByClassName("chord"))().concat(Ar
     let isSharp = false;
     if("C,Dm,Em,F,G,Am,Bm".split(",").includes(base)){};
     if("Cm,D,E,Fm,Gm,A,B".split(",").includes(base)){swapped = true;};
-    if(sharp == "#"){isSharp = true;};
+    if(keySignature == "#"){isSharp = true;};
     q = q.replace("7sus4","sus4").replace("dim7","dim").replace("7-5","m7-5");
     if("dim,7,add9,M7,sus4,aug,m7-5".split(",").includes(q)){isQAvailable = true;}else{if(q.length>0){unSupported = true;}};
     s = no + (isSharp?"[#]":"") + (swapped?"~":"") + (isQAvailable?("["+q+"]"):"" + (unSupported?"[!!"+q+"!!]":""));
