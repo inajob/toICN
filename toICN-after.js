6eaf1cfe2eb4b7b5e9f0116c9c37f5a2d63764df
nonoyu <tu@ts-uc.org>
2021-09-03 18:39:17 +0900

    add element when sharp and/or swap

--- toICN-after.js
+++ toICN-after.js
@@ -1,3 +1,12 @@
   let icn = module.exports(""+e.firstChild.nodeValue);
-  if(icn!="")e.firstChild.nodeValue = icn;
+  let isSharp = false;
+  let isSwap = false;
+  if(icn!=""){
+    e.firstChild.nodeValue = icn;
+    if(icn.includes("#")){isSharp = true;}
+    if(icn.includes("~")){isSwap = true;}
+    if(isSharp&&isSwap){e.classList.add("sharpswap");}
+    else if(isSharp&&!isSwap){e.classList.add("sharp");}
+    else if(!isSharp&&isSwap){e.classList.add("swap");}
+  }
 })
