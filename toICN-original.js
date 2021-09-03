08abdd90e91a56d113954c26bbfee4c43ee92d33
nonoyu <ts-uc@users.noreply.github.com>
2021-09-03 12:57:55 +0900

    Update toICN-original.js
    Co-authored-by: inajob <kinadu@zlab.co.jp>

--- toICN-original.js
+++ toICN-original.js
@@ -2,7 +2,7 @@ Array.prototype.slice.bind(document.getElementsByClassName("chord"))().concat(Ar
   let scale = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
   let ICNScale = ["1","1#","2","2#","3","4","4#","5","5#","6","6#","7"];
   let raw = ""+ e.firstChild.nodeValue;
-  let m = raw.match(/^([A-G]#|b{0,1})(m{0,1})([^/]*)/);
+  let m = raw.match(/^([A-G](#|b){0,1})(m{0,1})([^/]*)/);
   let s = "-";
   if(m){
     let base = m[1].replace("Db","C#").replace("Eb","D#").replace("Gb","F#").replace("Ab","G#").replace("Bb","A#");
