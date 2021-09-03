b7a48f984388dbd7bd4e5ea3b1d0938b9c2ebaaa
inajob <ina_job@hotmail.com>
2021-09-03 13:22:45 +0900

    fix rebase mistakes

--- toICN-core.js
+++ toICN-core.js
@@ -1,7 +1,6 @@
 module.exports = function(raw){
   let scale = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
   let ICNScale = ["1","1#","2","2#","3","4","4#","5","5#","6","6#","7"];
-  let raw = ""+ e.firstChild.nodeValue;
   let m = raw.match(/^([A-G](#|b){0,1})(m{0,1})([^/]*)/);
   let s = "";
   if(m){
