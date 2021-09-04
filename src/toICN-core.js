46cc149b396bef93cd101e743aaef982eafb2bd1
nonoyu <tu@ts-uc.org>
2021-09-04 13:21:31 +0900

    fix ~,9→7(9),add9→9,fix test.js

--- src/toICN-core.js
+++ src/toICN-core.js
@@ -18,7 +18,7 @@ module.exports = function(raw){
     }
     let no = ICNScale[scale.indexOf(base)];
     isSharp = no.includes("#");
-    q = q.replace(/^maj$/,"").replace(/^min$/,"m").replace(/^maj7$/,"M7").replace("7sus4","sus4").replace("dim7","dim").replace(/^m7b5|m7\(-5\)|m7\(b5\)$/,"m7-5");
+    q = q.replace(/^9$/,"7(9)").replace(/^add9$/,"9").replace(/^maj$/,"").replace(/^min$/,"m").replace(/^maj7$/,"M7").replace("7sus4","sus4").replace("dim7","dim").replace(/^m7b5|m7\(-5\)|m7\(b5\)$/,"m7-5");
     if("m,m7,mM7,m9,m6".split(",").includes(q)){
       minorSignature = "m";
       q = q.replace("m","");
@@ -26,8 +26,12 @@ module.exports = function(raw){
     if("1m,2,3,4m,5m,6,7,1#m,2#m,4#m,5#m,6#m".split(",").includes(no+minorSignature)){
       swapped = true;
     }
-    if("7,M7,add9,6,sus4,aug,dim,m7-5".split(",").includes(q)){
+    if("7,M7,9,6".split(",").includes(q)){
       isQAvailable = true;
+    }
+    if("sus4,aug,dim,m7-5".split(",").includes(q)){
+      isQAvailable = true;
+      swapped = false;
     }
     else{
       if(q.length>0){
