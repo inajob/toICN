2f7e65dd39313ca54c99213014583b25ac542e3a
inajob <ina_job@hotmail.com>
2021-09-04 20:16:04 +0900

    improve auto detect

--- src/toICN-after.js
+++ src/toICN-after.js
@@ -5,9 +5,9 @@ let chords = chordElms.map((e) => e.firstChild.nodeValue);
 detectedKey = "";
 if(key == ""){
   let maxCount = 0;
-  chords.forEach((s) => {
+  scale.forEach((s) => {
     key = s;
-    let notSwapCodesCount = chords.map((s) => module.exports(s)).filter((s) => !(/dim|m7-5|aug/).test(s)).filter((s) => (s.indexOf("~") == -1)).length;
+    let notSwapCodesCount = chords.map((s) => module.exports(s)).filter((s) => !(/dim|m7-5|aug/).test(s)).filter((s) => /^([123456][^#~]*$|3~[^#]*$)/.test(s)).length;
     if(notSwapCodesCount > maxCount){
       maxCount = notSwapCodesCount;
       detectedKey = key;
