a26906f17afa752164f444b489e397c494d6c141
nonoyu <tu@ts-uc.org>
2021-09-04 15:29:22 +0900

    edit README.sh

--- template/README.sh
+++ template/README.sh
@@ -27,6 +27,10 @@ ChordWikiかU-FRETで目的の曲を表示した状態で、登録したブッ�
 - ChordWikiにてKeyが明示されている場合は正しく動きますが、それ以外の場合はキーをC(ハ長調)と決め打ちします。
 - https://github.com/inajob/toICN/issues に随時起票しています。（コメント、PR募集してます！）
 
+# 仕様
+
+- ICN、およびこのブックマークレットの仕様については[こちらをご覧ください](/specification.md)。
+
 # 開発方法
 
 - toICN-before.js
