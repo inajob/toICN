9a6cb7f106b7ff2a1418a0f7ffc49e74a666419c
inajob <ina_job@hotmail.com>
2021-09-04 14:48:57 +0900

    fix multiple style tag

--- src/toICN-before.js
+++ src/toICN-before.js
@@ -1,5 +1,15 @@
 let keyElm = document.getElementsByClassName('key')[0];
 let keyMatch = keyElm?keyElm.firstChild.nodeValue.match(/Key: ([A-G](#|b){0,1})m{0,1}/):null;
 let key = keyMatch?keyMatch[1]:"C";
+let style = document.createElement('style');
+document.head.appendChild(style);
+let sheet = style.sheet;
+sheet.insertRule('.word {color:#b22222}');
+sheet.insertRule('.wordtop {color:#b22222}');
+sheet.insertRule('.sharp {background-color:#dbdbdb}');
+sheet.insertRule('.swap {background-color:#fab9bd}');
+sheet.insertRule('.sharpswap {background-color:#d19fa0}');
+sheet.insertRule('.bluechord {color:#1a4a9c !important}');
+sheet.insertRule('.notbluechord {color:#000000 !important}');
 Array.prototype.slice.bind(document.getElementsByClassName("chord"))().concat(Array.prototype.slice.bind(document.getElementsByTagName("rt"))()).forEach((e) => {
   module = {exports:{}};
