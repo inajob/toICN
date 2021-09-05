ff37078d48b9320f0a27ca3e7c7aa0977d1e0ef8
nonoyu <tu@ts-uc.org>
2021-09-05 11:59:41 +0900

    run gen.sh

--- toICN-debug.js
+++ toICN-debug.js
@@ -90,15 +90,19 @@ if(detectedKey == ""){
   });
   key = "";
   detectedKey = tmpDetectedKey;
-  alert("Auto Detect Key: " + detectedKey);
 }
+var result = prompt("自動検出されたキー:" + detectedKey + "\n別のキーを指定したい場合は、下にキーを入力してください。(例:C)\nよくわからなければ、そのままOKを押してください。");
+let resultMatch = result.match(/([A-G](#|b){0,1})(m{0,1})$/);
+let resultKey = resultMatch?keyMatch[1]:"";
+let resultKeyMinorSignature = resultMatch?keyMatch[3]:"";
+if(scale.includes(resultKey)){isAutoKeyDetection = false;}
 if(isAutoKeyDetection){
   key = detectedKey;
   keyMinorSignature = detectedKeyMinorSignature;
 }
 else{
-  key = "C";
-  keyMinorSignature = "";
+  key = resultKey;
+  keyMinorSignature = resultKeyMinorSignature;
 }
 //表示書き換え関係
 chordElms.forEach((e) => {
