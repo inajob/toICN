b09fa52efda57fdfbcea0074ece8800842a7c9d6
ts-uc <s0@ts-uc.org>
2021-09-14 20:45:32 +0900

    表示変更機能を追加

--- toICN-debug.js
+++ toICN-debug.js
@@ -212,44 +212,21 @@ function main () {
 
   // キーの手動設定
 
-  let barText = '';
-  barText += '<div class="toicnbar" style="background-color: #f4ffa2; margin-top: 5px; margin-bottom: 5px; padding: .75rem 1.25rem;">';
-  barText += '<div class="dispayedkey" style="font-weight: bold; font-size: 150%; color: #1a4a9c">';
-  barText += "Key: " + detectedKey.key;
-  barText += '</div>';
-  barText += '<label style = "display: inline-block;">Key:';
-  barText += '<select class="selectedkey" name="selectedkey">';
-  barText += '<option value=-1>Auto</option>';
-  barText += '<option value=0>C/Am</option>';
-  barText += '<option value=1>Db/Bbm</option>';
-  barText += '<option value=2>D/Bm</option>';
-  barText += '<option value=3>Eb/Cm</option>';
-  barText += '<option value=4>E/C#m</option>';
-  barText += '<option value=5>F/Dm</option>';
-  barText += '<option value=6>F#/D#m</option>';
-  barText += '<option value=7>G/Em</option>';
-  barText += '<option value=8>Ab/Fm</option>';
-  barText += '<option value=9>A/F#m</option>';
-  barText += '<option value=10>Bb/Gm</option>';
-  barText += '<option value=11>B/G#m</option>';
-  barText += '</select>';
-  barText += '</label>';
-  barText += '</div>';
-  
-  if(webSiteName == "ufret"){document.getElementById('my-chord-data').insertAdjacentHTML('beforebegin', barText);}
-  if(webSiteName == "chordwiki"){(document.getElementsByClassName('subtitle'))[0].insertAdjacentHTML('afterend', barText);}
-  if(webSiteName == "gakki.me"){document.getElementById('chord_area').insertAdjacentHTML('beforebegin', barText);}
-  if(webSiteName == "j-total"){document.body.insertAdjacentHTML('afterbegin', barText);}
-
   //表示書き換え関係
   exports.updateChords(keyChords, isAutoKeyDetection?detectedKey:specifiedKey, isAutoKeyDetection);
+  document.getElementById('displayedkey').innerText = "Original Key: " + detectedKey.key;
 
   document.querySelector('.selectedkey').addEventListener('change', (event) => {
     if(event.target.value == -1){ //Auto
       exports.updateChords(keyChords, detectedKey, true);
+      document.getElementById('displayedkey').innerText = "Original Key: " + detectedKey.key;
+      document.getElementById('toicnmessage').innerText = "";
     }
     else{
-      exports.updateChords(keyChords, new exports.Key(scale[event.target.value]), false);
+      let selectedKey = new exports.Key(scale[event.target.value]);
+      exports.updateChords(keyChords, selectedKey, false);
+      document.getElementById('displayedkey').innerText = "Key: " + selectedKey.key + " (selected)";
+      document.getElementById('toicnmessage').innerText = "# や ~ の付いたコードがたくさん表示される場合は、選択されたキーが正しくない可能性があります。その場合はKeyをAutoにしてください。";
     }
   });
 };
@@ -269,6 +246,37 @@ function waitElement(webSiteName, cb) {
     if (!!document.querySelector(selector)) resolve();
   }, 300);
 }
+
+let barText = '';
+barText += '<div class="toicnbar" style="background-color: #f4ffa2; margin-top: 5px; margin-bottom: 5px; padding: .75rem 1.25rem;">';
+barText += '<div id="displayedkey" style="font-weight: bold; font-size: 150%; color: #1a4a9c">';
+barText += '</div>';
+barText += '<label style = "display: inline-block;">Key:';
+barText += '<select class="selectedkey" name="selectedkey">';
+barText += '<option value=-1>Auto(推奨)</option>';
+barText += '<option value=0>C/Am</option>';
+barText += '<option value=1>Db/Bbm</option>';
+barText += '<option value=2>D/Bm</option>';
+barText += '<option value=3>Eb/Cm</option>';
+barText += '<option value=4>E/C#m</option>';
+barText += '<option value=5>F/Dm</option>';
+barText += '<option value=6>F#/D#m</option>';
+barText += '<option value=7>G/Em</option>';
+barText += '<option value=8>Ab/Fm</option>';
+barText += '<option value=9>A/F#m</option>';
+barText += '<option value=10>Bb/Gm</option>';
+barText += '<option value=11>B/G#m</option>';
+barText += '</select>';
+barText += '</label>';
+barText += '<div id="toicnmessage">';
+barText += '</div>';
+barText += '</div>';
+
+if(webSiteName == "ufret"){document.getElementById('my-chord-data').insertAdjacentHTML('beforebegin', barText);}
+if(webSiteName == "chordwiki"){(document.getElementsByClassName('subtitle'))[0].insertAdjacentHTML('afterend', barText);}
+if(webSiteName == "gakki.me"){document.getElementById('chord_area').insertAdjacentHTML('beforebegin', barText);}
+if(webSiteName == "j-total"){document.body.insertAdjacentHTML('afterbegin', barText);}
+
 
 if (document.readyState === 'complete' || document.readyState === 'interactive') {
   waitElement(webSiteName, main);
